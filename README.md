a48718fb5ce72927e99f25a1319c6ccb59160c4b
Matt Lim <mlimcollege@gmail.com>
2021-09-29 14:10:18 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Polygon Faucet Script
+
+Automates getting MATIC from https://faucet.polygon.technology/.
+
+Run the script like this:
+
+```
+node script.js NUM_TIMES ADDRESS
+```
+
+The faucet gives out 0.1 MATIC per transfer, and can only be called at most once per minute.
---
+++ script.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+async function tryOnce() {
+  const response = await axios.post(
+    "https://api.faucet.matic.network/transferTokens",
+    {
+      network: "mumbai",
+      address: process.argv[3],
+      token: "maticToken",
+    }
+  );
+
+  if (response.data.error?.includes("replacement transaction underpriced")) {
+    return tryOnce();
+  } else if (response.data.error?.includes("greylisted")) {
+    return "greylisted";
+  } else {
+    return "success";
+  }
+}
+
+async function tryMultiple(numTimes) {
+  console.log(`tryMultiple: numTimes = ${numTimes}`);
+  if (numTimes <= 0) {
+    return;
+  }
+
+  const tryOnceResult = await tryOnce();
+  console.log(`tryMultiple: tryOnceResult = ${tryOnceResult}`);
+  if (tryOnceResult === "success") {
+    // Try after 62 seconds
+    setTimeout(() => tryMultiple(numTimes - 1), 62000);
+  } else if (tryOnceResult === "greylisted") {
+    // Try after 62 seconds, don't decrement numTimes
+    setTimeout(() => tryMultiple(numTimes), 62000);
+  } else {
+    // Try again immediately
+    tryMultiple(numTimes);
+  }
+}
+
+tryMultiple(Number(process.argv[2]));
